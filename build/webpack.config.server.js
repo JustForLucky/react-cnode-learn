1e1e212e32538d309900dd1e0f1542f627f4f573
lili <751502445@qq.com>
2019-08-24 19:35:48 +0800

    客户端、服务端基础环境

---
+++ build/webpack.config.server.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    target: 'node',
+    entry: {
+        app: path.join(__dirname, '../client/server-entry.js')
+    },
+    output: {
+        filename: 'server-entry.js',
+        path: path.join(__dirname, '../dist'),
+        publicPath: 'public',
+        libraryTarget: 'commonjs2', // 最新commonjs模块方案
+    },
+    module: {
+        rules: [
+            {
+                test: /.jsx$/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /.js$/,
+                exclude: [
+                    path.join(__dirname, '../node_modules')
+                ],
+                loader: 'babel-loader',
+            }
+        ]
+    }
+}
