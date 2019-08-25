3f6030068077043142772558e600abfbfc0ceb7b
lili <751502445@qq.com>
2019-08-25 11:04:44 +0800

    路由配置

---
+++ client/views/topic-list/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class TopicList extends Component {
+  componentDidMount() {
+    // do something here
+
+  }
+  render() {
+    return (
+      <div>This is topic list </div>
+    )
+  }
+}
+
+export default TopicList
