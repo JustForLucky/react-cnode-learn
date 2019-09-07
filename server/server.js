4e7083f9ac326eee5531c1e61e0de1a0e6c56a5b
lili <751502445@qq.com>
2019-09-07 17:52:58 +0800

    服务端渲染完成

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const ReactSSR = require('react-dom/server');
+// const ReactSSR = require('react-dom/server');
 const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const fs = require('fs');
 const path = require('path');
+const serverRender = require('./util/server-render');
 
 const isDev = process.env.NODE_ENV === 'development';
 
@@ -25,17 +26,24 @@ app.use('/api/user', require('./util/handle-login').router);
 app.use('/api', require('./util/proxy'));
 
 if (!isDev) {
-    const serverEntry = require('../dist/server-entry').default;
-    const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8');
+    const serverEntry = require('../dist/server-entry');
+    const template = fs.readFileSync(path.join(__dirname, '../dist/server.ejs'), 'utf8');
     app.use('/public', express.static(path.join(__dirname, '../dist')))
-    app.get('*', function(req, res) {
-        const appString = ReactSSR.renderToString(serverEntry)
-        res.send(template.replace('<!-- app -->', appString));
+    app.get('*', function(req, res, next) {
+      serverRender(serverEntry, template, req, res)
+      .catch(next);
+        // const appString = ReactSSR.renderToString(serverEntry)
+        // res.send(template.replace('<!-- app -->', appString));
     })
 } else {
     const devStatic = require('./util/dev-static');
     devStatic(app);
 }
+// 根据参数长度判断是否是ErrorHandler
+app.use(function(error, req, res, next) {
+  console.log(error);
+  res.status(500).send(error);
+})
 
 app.listen(3000, function() {
     console.log('server is listening on 3000')
--- server/util/dev-static.js
+++ server/util/dev-static.js
@@ -3,12 +3,9 @@ const path = require('path');
 const webpack = require('webpack');
 const MemoryFs = require('memory-fs');
 const serverConfig = require('../../build/webpack.config.server');
-const ReactDomServer = require('react-dom/server')
 const proxy = require('http-proxy-middleware')
-const asyncBootstrap = require('react-async-bootstrapper').default;
-const ejs = require('ejs');
-const serialize = require('serialize-javascript');
-const Helmet = require('react-helmet').default;
+const serverRender = require('./server-render');
+
 
 const getTemplate = () => {
     return new Promise((resolve, reject) => {
@@ -40,7 +37,7 @@ const getModuleFromString = (bundle, filename) => {
 const mfs = new MemoryFs();
 const serverCompiler = webpack(serverConfig);
 serverCompiler.outputFileSystem = mfs;
-let serverBundle, createStoreMap;
+let serverBundle;
 serverCompiler.watch({}, (err, stats) => {
     if (err) throw err;
     stats = stats.toJson();
@@ -54,48 +51,20 @@ serverCompiler.watch({}, (err, stats) => {
     // const m = new Module;
     // m._compile(bundle, 'server-entry.js');
     const m = getModuleFromString(bundle, 'server-entry.js');
-    serverBundle = m.exports.default;
-    createStoreMap = m.exports.createStoreMap;
+    serverBundle = m.exports;
+    // createStoreMap = m.exports.createStoreMap;
 })
 
-const getStoreState = (stores) => {
-  return Object.keys(stores).reduce((result, storeName) => {
-    result[storeName] = stores[storeName].toJson();
-    return result;
-  }, {})
-}
 
 module.exports = function(app) {
     app.use('/public', proxy({
         target: 'http://localhost:8888'
     }))
-    app.get('*', function(req, res) {
+    app.get('*', function(req, res, next) {
         getTemplate()
         .then(template => {
-            const routerContext = {};
-            const stores = createStoreMap();
-            const app = serverBundle(stores,routerContext, req.url)
-            asyncBootstrap(app).then(() => {
-              if (routerContext.url) {
-                res.status(302).setHeader('Location', routerContext.url);
-                res.end();
-                return;
-              }
-              const helmet = Helmet.rewind();
-              const state = getStoreState(stores);
-              const content = ReactDomServer.renderToString(app);
-              const html = ejs.render(template, {
-                appString: content,
-                initialState: serialize(state),
-                meta: helmet.meta.toString(),
-                title: helmet.title.toString(),
-                style: helmet.style.toString(),
-                link: helmet.link.toString(),
-              })
-              res.send(html);
-              // res.send(template.replace('<!-- app -->', content));
-
-            })
+          return serverRender(serverBundle, template, req, res)
         })
+        .catch(next)
     })
 }
